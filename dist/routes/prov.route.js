f66a95cc59b54182747fa127116ec1396a25334a
MarcoParisi <marco.parisi@edu.itspiemonte.it>
2023-01-09 12:10:22 +0100

    deleted check

--- dist/routes/prov.route.js
+++ dist/routes/prov.route.js
@@ -6,10 +6,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const prov_controller_1 = require("../controller/prov.controller");
 const checkRegId_1 = __importDefault(require("../middleware/checkRegId"));
-const checkProvId_1 = __importDefault(require("../middleware/checkProvId"));
 const provRouter = express_1.default.Router();
 provRouter.route('/:region/prov')
     .get(checkRegId_1.default, prov_controller_1.getAllProv);
 provRouter.route('/:region/prov/:prov')
-    .get(checkRegId_1.default, checkProvId_1.default, prov_controller_1.getProv);
+    .get(checkRegId_1.default, prov_controller_1.getProv);
 exports.default = provRouter;
--- routes/prov.route.ts
+++ routes/prov.route.ts
@@ -1,7 +1,6 @@
 import express from 'express'
 import { getAllProv, getProv } from '../controller/prov.controller';
 import checkRegId from '../middleware/checkRegId';
-import checkProvId from '../middleware/checkProvId';
 
 const provRouter = express.Router();
 
@@ -9,6 +8,6 @@ provRouter.route('/:region/prov')
     .get(checkRegId, getAllProv)
 
 provRouter.route('/:region/prov/:prov')
-    .get(checkRegId, checkProvId, getProv)
+    .get(checkRegId, getProv)
 
 export default provRouter;
