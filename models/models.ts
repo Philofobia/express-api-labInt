576c410a7ef1fceb9e9a9d11909baacdf2cc8ec6
Marco Parisi <marco.parisi@edu.itspiemonte.it>
2023-01-12 15:16:26 +0100

    added data

--- models/models.ts
+++ models/models.ts
@@ -4,7 +4,15 @@ export interface RegioneInt {
   id: number;
   descrizione: string[];
   immagini: string[];
-  provincie: { nome: string; id: number, img: string }[];
+  provincie: { nome: string; id: number; img: string }[];
+  dataPres: {
+    indicatori: string;
+    alloggio: string;
+    paeseRes: string;
+    value: number;
+    anno: number;
+    mese: number;
+  }[];
 }
 
 export interface provsItalyInt {
